f4753b7a00cc9f08401763b23115db2204870e39
Edantozy <edantozy@hotmail.com>
2020-05-25 06:43:17 +0000

    Este es un segundo commit

--- pagina.php
+++ pagina.php
@@ -3,3 +3,17 @@
 $a = "Hola mundo";
 
  ?>	
+ <!DOCTYPE html>
+ <html lang="es">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Esta es una página web.</title>
+ </head>
+ <body>
+ 	<h1>Bienvenido!</h1>
+
+<?php echo "<h3>Esto es un código en PHP</h3>" ?>
+
+
+ </body>
+ </html>
